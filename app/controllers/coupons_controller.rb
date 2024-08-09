133171b14db85f0622c695257169510eae1cbc58
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-08-09 09:34:32 -0400

    Refactor controller

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -15,7 +15,6 @@ class CouponsController < ApplicationController
     @coupon = Coupon.new
     @coupon.coupon_code = params[:coupon][:coupon_code]
     @coupon.store = params[:coupon][:store]
-
     @coupon.save
     redirect_to @coupon
   end
