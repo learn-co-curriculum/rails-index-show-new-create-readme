414cecd8358e38ac71ae8211db5aa9f9a7b8f24b
Stephen McKeon <stephen.mckeon@powerhrg.com>
2024-08-12 10:24:52 -0400

    Add link to index

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -3,6 +3,12 @@ class CouponsController < ApplicationController
     @coupons = Coupon.all
   end
 
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new; end
+
   def create
     coupon = Coupon.create(coupon_params)
 
